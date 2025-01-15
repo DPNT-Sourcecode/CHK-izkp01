edd44028bd9f19ba0cc72afee8efe383c743f653
sbx_user1051 <sbx_user1051@169.254.113.138>
2025-01-15 13:48:52 +0000

    Wed Jan 15 13:48:52 UTC 2025

---
+++ test/solutions/CHK/test-checkout.js
@@ -0,0 +1,23 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const chk = require('../../../lib/solutions/CHK/checkout');
+
+describe('CHK challenge', function () {
+    it('should return correct total', function () {
+        assert.equal(chk("AAABCD"), 195);
+    });
+
+    it('should return correct total for only offers', function () {
+        assert.equal(chk("AAABB"), 175);
+    });
+
+    it('should return correct total for only one item', function () {
+        assert.equal(chk("A"), 50);
+    });
+
+    it('should return correct total for no offers', function () {
+        assert.equal(chk("ABCD"), 115);
+    });
+});
